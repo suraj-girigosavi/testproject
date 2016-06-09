59878e5d33e6183f168e682c6dbf4a4daba5ca1e
suraj <suraj@technoheaven.net>
2016-06-09 15:51:29 +0530

    addes new activity layout file

--- app/src/main/java/com/technoheaven/mytestgitapplication/ActivitySeond.java
+++ app/src/main/java/com/technoheaven/mytestgitapplication/ActivitySeond.java
@@ -11,5 +11,8 @@ public class ActivitySeond extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_second);
+
     }
 }
